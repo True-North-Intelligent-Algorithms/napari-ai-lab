7f32a0024fc1a99034b3698e359f8ef61af96385
bnorthan <bnorthan@gmail.com>
2025-11-10 09:45:22 -0500

    Remove auto-generated widget and writer

--- src/napari_ai_lab/__init__.py
+++ src/napari_ai_lab/__init__.py
@@ -2,21 +2,4 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-
-from ._widget import (
-    ExampleQWidget,
-    ImageThreshold,
-    threshold_autogenerate_widget,
-    threshold_magic_widget,
-)
-from ._writer import write_multiple, write_single_image
-
-__all__ = (
-    "write_single_image",
-    "write_multiple",
-    "ExampleQWidget",
-    "ImageThreshold",
-    "threshold_autogenerate_widget",
-    "threshold_magic_widget",
-    "NDEasySegment",
-)
+__all__ = ("NDEasySegment",)
